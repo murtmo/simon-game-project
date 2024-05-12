1da52289ff33d9b57d604409b2d109a5722accee
TamaoMiura <miuratamao.d@gmail.com>
2024-05-12 14:41:18 +0900

    check which button is pressed

--- game.js
+++ game.js
@@ -1,14 +1,23 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  // console.log(userClickedPattern);
+});
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
 
   gamePattern.push(randomChosenColour);
 
+  // add animation and sound
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 }
